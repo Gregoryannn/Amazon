6231b4430a23c6c82e812f301f33d802701f69a5
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-24 08:52:43 +0100

    Create Checkout component

--- src/App.js
+++ src/App.js
@@ -2,25 +2,25 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import Checkout from './components/Checkout';
 
 const App = () => {
     return (
-       
-        < Router >
-        <div className="app">
-            <Switch>
-                <Route path="/checkout">
-                    <Navbar />
+        <Router>
+            <div className="app">
+                <Switch>
+                    <Route path="/checkout">
+                        <Navbar />
 
-                </Route>
-                <Route path="/">
-                    <Navbar />
-                    <Home />
-                </Route>
-            </Switch>
-        </div>
-        </Router >
+                        <Checkout />
+                    </Route>
+                    <Route path="/">
+                        <Navbar />
+                        <Home />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     );
 };
-
 export default App;
