28d4e2b4fe595c523568428e0d657beb2dd5f01f
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-24 16:56:54 +0100

    Correct sources after reorganize components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import "./App.css";
 import Navbar from "./components/Navbar";
-import Home from "./components/Home";
-import Checkout from './components/Checkout';
+import Home from "./pages/Home";
+import Checkout from './pages/Checkout';
+
+
 const App = () => {
     return (
         <Router>
