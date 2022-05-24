a3678e45b77c4e207a05ba3a493b2b9ba63c07b2
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-24 08:51:45 +0100

    Add Route in App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 
 const App = () => {
     return (
+       
+        < Router >
         <div className="app">
-            <Navbar />
-            <Home />
+            <Switch>
+                <Route path="/checkout">
+                    <Navbar />
+
+                </Route>
+                <Route path="/">
+                    <Navbar />
+                    <Home />
+                </Route>
+            </Switch>
         </div>
-    )
+        </Router >
+    );
 };
+
 export default App;
