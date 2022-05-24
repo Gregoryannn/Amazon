94250caace1b1b6aad281b525f521e33e045d6b9
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-24 16:56:10 +0100

    Wrap the App with StateProvider

--- src/components/Checkout/index.js
+++ src/components/Checkout/index.js
@@ -1,25 +1,14 @@
-import React from 'react';
-import './Checkout.css';
-import Subtotal from '../../components/Subtotal';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { StateProvider } from "./StateProvider";
 
-const Checkout = () => {
-    return (
-        <div className="checkout">
-            <div className="checkout__left">
-                <img
-                    className="checkout__ad"
-                    src="https://images-na.ssl-images-amazon.com/images/G/15/dex/2020/PrimeDelivery/DEX_2020_PrimeDelivery_LP_CA_0407_Header_Desktop_3000x600.jpg"
-                    alt="Currency Converter ad banner"
-                />
-                <div>
-                    <h2 className="checkout__title">Your shopping Basket</h2>
-                </div>
-            </div>
-
-            <div className="checkout__right">
-                <Subtotal />
-            </div>
-        </div>
-    );
-}
-export default Checkout;
+ReactDOM.render(
+    <React.StrictMode>
+          <StateProvider initialState={initialState} reducer={reducer}>
+            <App />
+        </StateProvider>
+    </React.StrictMode>,
+    document.getElementById("root")
+);
