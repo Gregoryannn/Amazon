c95e288c656fc05a0e7097cfacc14acd04282120
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-24 08:57:50 +0100

    Build Checkout component

--- src/components/Checkout/index.js
+++ src/components/Checkout/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import './Checkout.css';
-
 const Checkout = () => {
     return (
         <div className="checkout">
+            <div className="checkout__left">
+                <img
+                    className="checkout__ad"
+                    src="https://images-na.ssl-images-amazon.com/images/G/15/dex/2020/PrimeDelivery/DEX_2020_PrimeDelivery_LP_CA_0407_Header_Desktop_3000x600.jpg"
+                    alt="Currency Converter ad banner"
+                />
 
+                <div>
+                    <h2 className="checkout__title">Your shopping Basket</h2>
+                </div>
+            </div>
+
+            <div className="checkout__right">
+                <h2>The subtotal will go here</h2>
+            </div>
         </div>
-    )
+
+   );
 }
 
 export default Checkout;
