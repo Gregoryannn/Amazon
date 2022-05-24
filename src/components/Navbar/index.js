ba1dd2b422a21e487004736e6ad3f5f6ee2c288c
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-24 08:56:33 +0100

    Add Links to logo and shopping basket

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
@@ -6,16 +7,18 @@ import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 const Navbar = () => {
     return (
         <div className="nav">
-            <img
-                className="nav__logo"
-                src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-            />
+    
+            <Link to="/">
+                <img
+                    className="nav__logo"
+                    src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                />
+            </Link>
 
             <div className="nav__search">
                 <input className="nav__searchInput" type="text" />
                 <SearchIcon className="nav__searchIcon" />
             </div>
-
             <div className="nav__right">
                 <div className="nav__right__option">
                     <span className="nav__right__optionLineOne">
@@ -23,26 +26,25 @@ const Navbar = () => {
                     </span>
                     <span className="nav__right__optionLineTwo">Sign In</span>
                 </div>
-
                 <div className="nav__right__option">
                     <span className="nav__right__optionLineOne">Returns</span>
                     <span className="nav__right__optionLineTwo">& Orders</span>
                 </div>
-
                 <div className="nav__right__option">
                     <span className="nav__right__optionLineOne">Your</span>
                     <span className="nav__right__optionLineTwo">Prime</span>
                 </div>
 
-                <div className="nav__right__optionBasket">
-                    <ShoppingBasketIcon />
-                    <span className="nav__right__optionLineTwo nav__right__basketCount">
-                        0
-                    </span>
-                </div>
+                <Link to="/checkout">
+                    <div className="nav__right__optionBasket">
+                        <ShoppingBasketIcon />
+                        <span className="nav__right__optionLineTwo nav__right__basketCount">
+                            0
+                        </span>
+                    </div>
+                </Link>
             </div>
         </div>
     );
 }
-
 export default Navbar;
