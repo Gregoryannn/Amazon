c3c3b15052870ff0260f38e9a31ce31fecc97560
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-25 07:48:27 +0100

    Dispatch the items to the basket

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,27 +1,46 @@
 ﻿import React from 'react';
+import { useStateValue } from '../../StateProvider';
 import './Product.css';
 
-  const Product = ({ id, title, image, price, rating }) => {
-        return (
-            <div className="product">
-                <div className="product__info">
-                    <p>{title}</p>
-                    <p className="product__price">
-                        <small>$</small>
-                        <strong>{price}</strong>
-                    </p>
-                    <div className="product__rating">
-                        {Array(rating).fill().map((_, i) => (
-                            <p>⭐️</p>
-                        ))}
-                    </div>
-                </div>
-                <img
-                    src={image}
-                    alt={title}
-                />
-                <button>Add to Basket</button>
-            </div>
-        );
+const Product = ({ id, title, image, price, rating }) => {
+    const [{ basket }, dispatch] = useStateValue();
+
+    console.log('basket', basket)
+
+    const addToBasket = () => {
+        // dispatch the item into the data layer
+        dispatch({
+            type: 'ADD_TO_BASKET',
+            item: {
+                id,
+                title,
+                image,
+                price,
+                rating,
+            }
+        })
     }
-    export default Product;
+
+    return (
+        <div className="product">
+            <div className="product__info">
+                <p>{title}</p>
+                <p className="product__price">
+                    <small>$</small>
+                    <strong>{price}</strong>
+                </p>
+                <div className="product__rating">
+                    {Array(rating).fill().map((_, i) => (
+                        <p>⭐️</p>
+                    ))}
+                </div>
+            </div>
+            <img
+                src={image}
+                alt={title}
+            />
+            <button onClick={addToBasket}>Add to Basket</button>
+        </div>
+    );
+}
+export default Product;
--- src/reducer.js
+++ src/reducer.js
@@ -3,8 +3,10 @@ export const initialState = {
 }
 
 const reducer = (state, action) => {
+    console.log(action);
+
     switch (action.type) {
-        case "ADD_TO_Basket":
+        case "ADD_TO_BASKET":
             return {
                 ...state,
                 basket: [...state.basket, action.item],
@@ -13,5 +15,4 @@ const reducer = (state, action) => {
             return state;
     }
 };
-
 export default reducer;
