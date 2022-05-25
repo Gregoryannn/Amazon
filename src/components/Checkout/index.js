6c72549c14927cfa4e46d75eb09b67e7b156917b
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-25 07:53:16 +0100

    Create CheckoutProduct component

--- src/components/Checkout/index.js
+++ src/components/Checkout/index.js
@@ -1,14 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { StateProvider } from "./StateProvider";
+import React from 'react';
+import './Checkout.css';
+import Subtotal from '../../components/Subtotal';
+import CheckoutProduct from '../../components/CheckoutProduct';
 
-ReactDOM.render(
-    <React.StrictMode>
-          <StateProvider initialState={initialState} reducer={reducer}>
-            <App />
-        </StateProvider>
-    </React.StrictMode>,
-    document.getElementById("root")
-);
+const Checkout = () => {
+    return (
+        <div className="checkout">
+            <div className="checkout__left">
+                <img
+                    className="checkout__ad"
+                    src="https://images-na.ssl-images-amazon.com/images/G/15/dex/2020/PrimeDelivery/DEX_2020_PrimeDelivery_LP_CA_0407_Header_Desktop_3000x600.jpg"
+                    alt="Currency Converter ad banner"
+                />
+
+                <div>
+                    <h2 className="checkout__title">Your shopping Basket</h2>
+                    <CheckoutProduct />
+                </div>
+            </div>
+
+            <div className="checkout__right">
+                <Subtotal />
+            </div>
+        </div>
+    );
+}
+export default Checkout;
---
+++ src/components/CheckoutProduct/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./CheckoutProduct.css";
+
+const CheckoutProduct = ({ id, image, title, price, rating }) => {
+    return (
+        <div className="checkoutProduct">
+
+        </div>
+    );
+};
+
+export default CheckoutProduct;
