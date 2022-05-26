ac1857cb228591703d3f876f9cf5fb38fb4b2003
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-26 07:59:00 +0100

    Link Sign in in the Navbar to the Login page

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -8,46 +8,61 @@ import { useStateValue } from "../../StateProvider";
 const Navbar = () => {
     const [{ basket }, dispatch] = useStateValue();
 
-    return (
-        <div className="nav">
-            <Link to="/">
-                <img
-                    className="nav__logo"
-                    src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-                />
-            </Link>
-            <div className="nav__search">
-                <input className="nav__searchInput" type="text" />
-                <SearchIcon className="nav__searchIcon" />
-            </div>
-            <div className="nav__right">
-                <div className="nav__right__option">
-                    <span className="nav__right__optionLineOne">
-                        Hello Guest
-                    </span>
-                    <span className="nav__right__optionLineTwo">Sign In</span>
-                </div>
-                <div className="nav__right__option">
-                    <span className="nav__right__optionLineOne">Returns</span>
-                    <span className="nav__right__optionLineTwo">& Orders</span>
-                </div>
-                <div className="nav__right__option">
-                    <span className="nav__right__optionLineOne">Your</span>
-                    <span className="nav__right__optionLineTwo">Prime</span>
-                </div>
-                <Link to="/checkout">
-                    <div className="nav__right__optionBasket">
-                        <ShoppingBasketIcon />
-                        <span className="nav__right__optionLineTwo nav__right__basketCount">
-                              {basket?.length}
-                        </span>
-                    </div>
-                </Link>
-            </div>
-        </div>
+    return ( <
+        div className = "nav" >
+        <
+        Link to = "/" >
+        <
+        img className = "nav__logo"
+        src = "http://pngimg.com/uploads/amazon/amazon_PNG11.png" /
+        >
+        <
+        /Link> <
+        div className = "nav__search" >
+        <
+        input className = "nav__searchInput"
+        type = "text" / >
+        <
+        SearchIcon className = "nav__searchIcon" / >
+        <
+        /div> <
+        div className = "nav__right" >
+        <
+        Link to = "/login" >
+        <
+        div className = "nav__right__option" >
+        <
+        span className = "nav__right__optionLineOne" >
+        Hello Guest <
+        /span> <
+        span className = "nav__right__optionLineTwo" > Sign In < /span> <
+        /div> <
+        /Link> <
+        div className = "nav__right__option" >
+        <
+        span className = "nav__right__optionLineOne" > Returns < /span> <
+        span className = "nav__right__optionLineTwo" > & Orders < /span> <
+        /div> <
+        div className = "nav__right__option" >
+        <
+        span className = "nav__right__optionLineOne" > Your < /span> <
+        span className = "nav__right__optionLineTwo" > Prime < /span> <
+        /div> <
+        Link to = "/checkout" >
+        <
+        div className = "nav__right__optionBasket" >
+        <
+        ShoppingBasketIcon / >
+        <
+        span className = "nav__right__optionLineTwo nav__right__basketCount" > { basket ? .length } <
+        /span> <
+        /div> <
+        /Link> <
+        /div> <
+        /div>
     );
 }
 export default Navbar;
-    );
+);
 }
 export default Navbar;
