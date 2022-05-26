e12a028b33933d45f976d546a7deb14c992e5e7f
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-26 07:59:41 +0100

    Remove Navbar from the global Router

--- src/App.js
+++ src/App.js
@@ -4,25 +4,36 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Checkout from './pages/Checkout';
 import Login from './pages/Login';
-
 const App = () => {
-    return (
-        <Router>
-            <div className="app">
-                <Navbar />
-                <Switch>
-                    <Route path="/login">
-                        <Login />
-                    </Route>
-                    <Route path="/checkout">
-                        <Checkout />
-                    </Route>
-                    <Route path="/">
-                        <Home />
-                    </Route>
-                </Switch>
-            </div>
-        </Router>
+    return ( <
+        Router >
+        <
+        div className = "app" >
+        <
+        Switch >
+        <
+        Route path = "/login" >
+        <
+        Login / >
+        <
+        /Route> <
+        Route path = "/checkout" >
+        <
+        Navbar / >
+        <
+        Checkout / >
+        <
+        /Route> <
+        Route path = "/" >
+        <
+        Navbar / >
+        <
+        Home / >
+        <
+        /Route> <
+        /Switch> <
+        /div> <
+        /Router>
     );
 };
 export default App;
