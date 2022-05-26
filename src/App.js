fe4c686d2ea784a67de8dbffc137965cddc1d77c
Grzegorz Wlochinski <32854050+Gregoryannn@users.noreply.github.com>
2022-05-26 15:17:55 +0100

    Store user in the data layer

--- src/App.js
+++ src/App.js
@@ -1,39 +1,52 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import Checkout from './pages/Checkout';
-import Login from './pages/Login';
+import Checkout from "./pages/Checkout";
+import Login from "./pages/Login";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
+
 const App = () => {
-    return ( <
-        Router >
-        <
-        div className = "app" >
-        <
-        Switch >
-        <
-        Route path = "/login" >
-        <
-        Login / >
-        <
-        /Route> <
-        Route path = "/checkout" >
-        <
-        Navbar / >
-        <
-        Checkout / >
-        <
-        /Route> <
-        Route path = "/" >
-        <
-        Navbar / >
-        <
-        Home / >
-        <
-        /Route> <
-        /Switch> <
-        /div> <
-        /Router>
+    const [{}, dispatch] = useStateValue();
+
+    useEffect(() => {
+        auth.onAuthStateChanged((authUser) => {
+            console.log("the user is: ", authUser);
+
+            if (authUser) {
+                dispatch({
+                    type: "SET_USER",
+                    user: authUser,
+                });
+            } else {
+                dispatch({
+                    type: "SET_USER",
+                    user: null,
+                });
+            }
+        });
+    }, []);
+
+    return (
+        <Router>
+            <div className="app">
+                <Switch>
+                    <Route path="/login">
+                        <Login />
+                    </Route>
+                    <Route path="/checkout">
+                        <Navbar />
+                        <Checkout />
+                    </Route>
+                    <Route path="/">
+                        <Navbar />
+                        <Home />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     );
 };
 export default App;
