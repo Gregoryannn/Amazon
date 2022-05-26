1723e2f097d2517532b654d3b6f8d5f8683f3fda
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-26 08:02:18 +0100

    Add sign In and register function

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,21 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Login.css';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Login.css";
+
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const signIn = e => {
+        e.preventDefault();
+
+    };
+
+    const register = e => {
+        e.preventDefault();
+
+    }
+
     return ( <
         div className = "login" >
         <
@@ -12,9 +26,7 @@ const Login = () => {
         alt = "Amazon logo" /
         >
         <
-        /Link>
-
-        <
+        /Link> <
         div className = "login__container" >
         <
         h1 > Sign in < /h1>
@@ -23,15 +35,28 @@ const Login = () => {
         form >
         <
         h5 > E - mail < /h5> <
-        input type = "text" / >
+        input type = "text"
+        value = { email }
+        onChange = {
+            (e) => setEmail(e.target.value) }
+        />
 
         <
         h5 > Password < /h5> <
-        input type = "password" / >
+        input type = "password"
+        value = { password }
+        onChange = {
+            (e) => setPassword(e.target.value) }
+        />
 
         <
-        button className = "login__signInButton" > Sign In < /button> <
+        button className = "login__signInButton"
+        type = "submit"
+        onClick = { signIn } >
+        Sign In <
+        /button> <
         /form>
+
 
         <
         p >
@@ -40,10 +65,15 @@ const Login = () => {
 
         <
         button className = "login__registerButton" >
+        <
+        button className = "login__registerButton"
+        onClick = { register } >
         Create your Amazon Clone account <
         /button> <
         /div> <
         /div>
     );
 }
+};
+
 export default Login;
