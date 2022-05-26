db0365a823ad9dfd813ca9e87173ab61b284dd8b
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-26 08:00:27 +0100

    Add amazon logo to login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Login.css';
 
 const Login = () => {
-    return (
-        <div>
+    return ( <
+        div className = "login" >
+        <
+        Link to = "/" >
+        <
+        img className = "login__logo"
+        src = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+        alt = "Amazon logo" /
+        >
+        <
+        /Link> <
+        /div>
 
-        </div>
-    )
+    );
 }
 
 export default Login;
