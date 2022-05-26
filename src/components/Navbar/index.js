e3fe7c95c7c2eda35a190c8c0e4da1c2b886278e
Grzegorz Wlochinski <32854050+Gregoryannn@users.noreply.github.com>
2022-05-26 15:19:44 +0100

    Finish user authentication with firebase

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -4,65 +4,57 @@ import "./Navbar.css";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 import { useStateValue } from "../../StateProvider";
+import {auth} from '../../firebase';
 
 const Navbar = () => {
-    const [{ basket }, dispatch] = useStateValue();
+    const [{ basket, user }, dispatch] = useStateValue();
 
-    return ( <
-        div className = "nav" >
-        <
-        Link to = "/" >
-        <
-        img className = "nav__logo"
-        src = "http://pngimg.com/uploads/amazon/amazon_PNG11.png" /
-        >
-        <
-        /Link> <
-        div className = "nav__search" >
-        <
-        input className = "nav__searchInput"
-        type = "text" / >
-        <
-        SearchIcon className = "nav__searchIcon" / >
-        <
-        /div> <
-        div className = "nav__right" >
-        <
-        Link to = "/login" >
-        <
-        div className = "nav__right__option" >
-        <
-        span className = "nav__right__optionLineOne" >
-        Hello Guest <
-        /span> <
-        span className = "nav__right__optionLineTwo" > Sign In < /span> <
-        /div> <
-        /Link> <
-        div className = "nav__right__option" >
-        <
-        span className = "nav__right__optionLineOne" > Returns < /span> <
-        span className = "nav__right__optionLineTwo" > & Orders < /span> <
-        /div> <
-        div className = "nav__right__option" >
-        <
-        span className = "nav__right__optionLineOne" > Your < /span> <
-        span className = "nav__right__optionLineTwo" > Prime < /span> <
-        /div> <
-        Link to = "/checkout" >
-        <
-        div className = "nav__right__optionBasket" >
-        <
-        ShoppingBasketIcon / >
-        <
-        span className = "nav__right__optionLineTwo nav__right__basketCount" > { basket ? .length } <
-        /span> <
-        /div> <
-        /Link> <
-        /div> <
-        /div>
+    const handleAuthentication = () => {
+        if (user) {
+            auth.signOut();
+        }
+    }
+
+    return (
+        <div className="nav">
+            <Link to="/">
+                <img
+                    className="nav__logo"
+                    src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                />
+            </Link>
+            <div className="nav__search">
+                <input className="nav__searchInput" type="text" />
+                <SearchIcon className="nav__searchIcon" />
+            </div>
+            <div className="nav__right">
+           <Link to={!user && "/login"} >
+                    <div onClick={handleAuthentication} className="nav__right__option">
+                        <span className="nav__right__optionLineOne">
+                                 Hello { user? user.email : `Guest`}
+                        </span>
+                    <span className="nav__right__optionLineTwo">{!user ? `Sign In`: `Sign Out`}</span>
+                    </div>
+                </Link>
+
+                <div className="nav__right__option">
+                    <span className="nav__right__optionLineOne">Returns</span>
+                    <span className="nav__right__optionLineTwo">& Orders</span>
+                </div>
+                <div className="nav__right__option">
+                    <span className="nav__right__optionLineOne">Your</span>
+                    <span className="nav__right__optionLineTwo">Prime</span>
+                </div>
+                <Link to="/checkout">
+                    <div className="nav__right__optionBasket">
+                        <ShoppingBasketIcon />
+                        <span className="nav__right__optionLineTwo nav__right__basketCount">
+                            {basket?.length}
+                        </span>
+                    </div>
+                </Link>
+            </div>
+        </div>
     );
-}
-export default Navbar;
-);
 }
 export default Navbar;
