95b9a9fc62489051b9e46054c219f631f405ccb7
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-26 08:00:54 +0100

    Build Login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Login.css';
-
 const Login = () => {
     return ( <
         div className = "login" >
@@ -13,10 +12,38 @@ const Login = () => {
         alt = "Amazon logo" /
         >
         <
-        /Link> <
-        /div>
+        /Link>
 
+        <
+        div className = "login__container" >
+        <
+        h1 > Sign in < /h1>
+
+        <
+        form >
+        <
+        h5 > E - mail < /h5> <
+        input type = "text" / >
+
+        <
+        h5 > Password < /h5> <
+        input type = "password" / >
+
+        <
+        button className = "login__signInButton" > Sign In < /button> <
+        /form>
+
+        <
+        p >
+        By singing - in you agree to the AMAZON CLONE Conditions of Use & Sale.Please see our Privacy Notice, our Cookies Notice and our Interest - Based Ads <
+        /p>
+
+        <
+        button className = "login__registerButton" >
+        Create your Amazon Clone account <
+        /button> <
+        /div> <
+        /div>
     );
 }
-
 export default Login;
