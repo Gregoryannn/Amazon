3308afb024993d41572a132510e88cf05f7de5f0
Grzegorz Wlochinski <32854050+Gregoryannn@users.noreply.github.com>
2022-05-26 15:21:07 +0100

    Show user email on checkout page

--- src/components/Checkout/index.js
+++ src/components/Checkout/index.js
@@ -5,7 +5,7 @@ import CheckoutProduct from "../../components/CheckoutProduct";
 import { useStateValue } from "../../StateProvider";
 
 const Checkout = () => {
-    const [{ basket }, patch] = useStateValue();
+    const [{ basket, user }, dispatch] = useStateValue();
 
     return (
         <div className="checkout">
@@ -13,12 +13,12 @@ const Checkout = () => {
                 <img
                     className="checkout__ad"
                     src="https://images-na.ssl-images-amazon.com/images/G/15/dex/2020/PrimeDelivery/DEX_2020_PrimeDelivery_LP_CA_0407_Header_Desktop_3000x600.jpg"
-                    alt="Currency Converter ad banner"
+                    alt="Currency Converter advertise banner"
                 />
 
                 <div>
+                <h3>Hello, {user?.email}</h3>
                     <h2 className="checkout__title">Your shopping Basket</h2>
-          
 
                     {basket.map(item => (
                         <CheckoutProduct
@@ -31,13 +31,10 @@ const Checkout = () => {
                     ))}
                 </div>
             </div>
-
             <div className="checkout__right">
                 <Subtotal />
             </div>
         </div>
     );
-
 };
-
 export default Checkout;
