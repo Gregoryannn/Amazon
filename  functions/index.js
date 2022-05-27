c2000794edebd782a774b779ee559e29dfcba74a
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-27 16:13:47 +0100

    Set up express api

---  functions/index.js
+++  functions/index.js
@@ -1,5 +1,33 @@
 const functions = require("firebase-functions");
 
+const express = require('express');
+const cors = require('cors');
+const stripe = require("stripe")(
+    "sk_test_51HSDKsDgq6i9SALTyPxDxZcuGyzVKcJtqiC0TB6aLNzfHWsE3uCl0r12ACG9pkVTmv3iDiZdOjEX56GIlLdjnbOD008Q4dKFt0"
+);
+// API
+
+// App config
+const app = express();
+
+// Middlewares
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+// API route
+app.get('/', (req, res) => {
+    res.status(200).send('HELLO')
+});
+
+
+// Listen command
+exports.api = functions.https.onRequest(app);
+
+// NOTE: example endpoint
+// http://localhost:5001/clone-b271c/us-central1/api
+
+
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
