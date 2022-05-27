7dc1b3d6432a425bcdc70d37d8253af3f5921a0e
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-27 15:38:05 +0100

    Correct typo

--- src/pages/Payment/index.js
+++ src/pages/Payment/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import axios from "../../axios";
 import "./Payment.css";
 import { useStateValue } from "../../StateProvider";
 import CheckoutProduct from "../../components/CheckoutProduct";
