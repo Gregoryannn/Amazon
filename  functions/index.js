78bf3cab0f58eaa0ebecc9075ecc9dbbe9fbee56
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-27 16:21:02 +0100

    Add post payment route

---  functions/index.js
+++  functions/index.js
@@ -1,10 +1,10 @@
 const functions = require("firebase-functions");
-
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const stripe = require("stripe")(
     "sk_test_51HSDKsDgq6i9SALTyPxDxZcuGyzVKcJtqiC0TB6aLNzfHWsE3uCl0r12ACG9pkVTmv3iDiZdOjEX56GIlLdjnbOD008Q4dKFt0"
 );
+
 // API
 
 // App config
@@ -14,24 +14,27 @@ const app = express();
 app.use(cors({ origin: true }));
 app.use(express.json());
 
-// API route
-app.get('/', (req, res) => {
-    res.status(200).send('HELLO')
-});
 
+// API routes
+app.get("/", (request, response) => response.status(200).send("hello world"));
+
+app.post("/payments/create", async (request, response) => {
+    const total = request.query.total;
+
+    console.log("Payment Request Received: ", total);
+
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: total,
+        currency: "cad",
+    });
+
+    response.status(201).send({
+        clientSecret: paymentIntent.client_secret,
+    });
+});
 
 // Listen command
 exports.api = functions.https.onRequest(app);
 
 // NOTE: example endpoint
 // http://localhost:5001/clone-b271c/us-central1/api
-
-
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
