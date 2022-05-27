7973021fd9ac786e385414afd47e42616c1f3162
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-27 11:43:22 +0100

    Build Strip from, complete function & show error

--- src/pages/Payment/index.js
+++ src/pages/Payment/index.js
@@ -1,71 +1,121 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Payment.css";
 import { useStateValue } from "../../StateProvider";
 import CheckoutProduct from "../../components/CheckoutProduct";
 import { Link } from "react-router-dom";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import CurrencyFormat from "react-currency-format";
+import { getBasketTotal } from "../../reducer";
 
 const Payment = () => {
-    const [{ user, basket }, dispatch] = useStateValue();
-    return ( <
-        div className = "payment" >
-        <
-        div className = "payment__container" >
-        <
-        h1 >
-        Checkout( < Link to = "/checkout" > { basket ? .length }
-            items < /Link>
-        ) <
-        /h1>
+        const [{ user, basket }, dispatch] = useStateValue();
+        const stripe = useStripe();
+        const elements = useElements();
 
-        <
-        div className = "payment__section" >
-        <
-        div className = "payment__title" >
-        <
-        h3 > Delivery Address < /h3> <
-        /div> <
-        div className = "payment__address" >
-        <
-        p > { user ? .email } < /p> <
-        p > 101 Hello React St. < /p> <
-        p > BC, Canada < /p> <
-        /div> <
-        /div>
+        const [succeeded, setSucceeded] = useState(false);
+        const [processing, setProcessing] = useState("");
+        const [error, setError] = useState(null);
+        const [disabled, setDisabled] = useState(true);
 
+        const handleSubmit = (e) => {};
 
-        <
-        div className = "payment__section" >
-        <
-        div className = "payment__title" >
-        <
-        h3 > Review items and delivery < /h3> <
-        /div> <
-        div className = "payment__items" > {
-            basket.map((item) => ( <
-                CheckoutProduct id = { item.id }
-                title = { item.title }
-                image = { item.image }
-                price = { item.price }
-                rating = { item.rating }
-                />
-            ))
-        } <
-        /div> <
-        /div> <
-        div className = "payment__section" >
-        <
-        div className = "payment__title" >
-        <
-        h3 > Payment method < /h3> <
-        /div>
+        const handleChange = (event) => {
+            setDisabled(event.empty);
+            setError(event.error ? event.error.message : "");
+        };
 
-        <
-        div className = "payment__details" > < /div> <
-        /div> <
-        /div> <
-        /div>
-    );
+        return ( <
+            div className = "payment" >
+            <
+            div className = "payment__container" >
+            <
+            h1 >
+            Checkout( < Link to = "/checkout" > { basket ? .length }
+                items < /Link>
+            ) <
+            /h1> <
+            div className = "payment__section" >
+            <
+            div className = "payment__title" >
+            <
+            h3 > Delivery Address < /h3> <
+            /div> <
+            div className = "payment__address" >
+            <
+            p > { user ? .email } < /p> <
+            p > 101 Hello React St. < /p> <
+            p > BC, Canada < /p> <
+            /div> <
+            /div> <
+            div className = "payment__section" >
+            <
+            div className = "payment__title" >
+            <
+            h3 > Review items and delivery < /h3> <
+            /div> <
+            div className = "payment__items" > {
+                basket.map((item) => ( <
+                    CheckoutProduct id = { item.id }
+                    title = { item.title }
+                    image = { item.image }
+                    price = { item.price }
+                    rating = { item.rating }
+                    />
+                ))
+            } <
+            /div> <
+            /div> <
+            div className = "payment__section" >
+            <
+            div className = "payment__title" >
+            <
+            h3 > Payment method < /h3> <
+            /div> <
+            div className = "payment__details" >
+            <
+            form action = "" >
+            <
+            CardElement / >
+            <
+            form onSubmit = { handleSubmit } >
+            <
+            CardElement onChange = { handleChange }
+            /> <
+            div className = "payment__priceContainer" >
+            <
+            CurrencyFormat renderText = {
+                (value) => ( <
+                    h3 > Order Total: { value } < /h3>
+                )
+            }
+            decimalScale = { 2 }
+            value = { getBasketTotal(basket) }
+            displayType = { "text" }
+            thousandSeparator = { true }
+            prefix = { "$" }
+            /> <
+            button disabled = {
+                processing || disabled || succeeded
+            } >
+            <
+            span > {
+                processing ? ( <
+                    p > Processing < /p>
+                ) : (
+                    "Buy Now"
+                )
+            } <
+            /span> <
+            /button> <
+            /div>
 
-};
-
-export default Payment;
+            {
+                error && < div > { error } < /div>} <
+                    /form> <
+                    /div> <
+                    /div> <
+                    /div> <
+                    /div>
+            );
+        };
+        export default Payment;
