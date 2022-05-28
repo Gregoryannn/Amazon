d199787689f2757cc4b27816c495936301d65008
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-28 08:09:04 +0100

    Update baseURL to deployed backend

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const instance = axios.create({
     // api (cloud functions) URL
-    baseURL: "http://localhost:5001/clone-b271c/us-central1/api",
+    baseURL: "https://us-central1-clone-b271c.cloudfunctions.net/api"
+    // "http://localhost:5001/clone-b271c/us-central1/api",
 });
 
 export default instance; 
