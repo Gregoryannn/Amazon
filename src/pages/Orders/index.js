7cb5db4b7ebfd4d21fc3745f9af72de941951a93
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-28 07:57:23 +0100

    Pull the order history belong to the user from the database

--- src/pages/Orders/index.js
+++ src/pages/Orders/index.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { db } from "../../firebase";
+import { useStateValue } from "../../StateProvider";
 import './Orders.css';
 
 const Orders = () => {
+    const [{ basket, user }, dispatch] = useStateValue();
+    const [orders, setOrders] = useState([]);
+
+    useEffect(() => {
+        if (user) {
+            db
+                .collection('users')
+                .doc(user?.uid)
+                .collection('orders')
+                .orderBy('created', 'desc')
+                .onSnapshot(snapshot => (
+                    setOrders(snapshot.docs.map(doc => ({
+                        id: doc.id,
+                        data: doc.data()
+                    })))
+                ))
+        } else { setOrders([]) }
+
+    }, []);
+
     return (
         <div className="orders">
 
+            <h1>Your Orders</h1>
         </div>
     )
 }
-
 export default Orders;
