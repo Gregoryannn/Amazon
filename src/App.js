a9f5a26ff8770a1f04ecd50d250877a986baefe9
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-28 07:55:44 +0100

    Add orders route and component in App.js

--- src/App.js
+++ src/App.js
@@ -9,17 +9,16 @@ import Login from "./pages/Login";
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
 import Payment from "./pages/Payment";
+import Orders from "./pages/Orders";
 
 const promise = loadStripe(
     "pk_test_51HSDKsDgq6i9SALTKlrxpCxlYE6jmxmkwr9XrDLrXenzpR2LTRCoacZu9vc00R5DzoMgUTsddly7nf2SDXD9lUjP00Cb7l87Ia"
 );
-
 const App = () => {
-    const [{}, dispatch] = useStateValue();
-
+    const [{ }, dispatch] = useStateValue();
     useEffect(() => {
         auth.onAuthStateChanged((authUser) => {
-            console.log("the user is: ", authUser);
+            // console.log("user: ", authUser);
             if (authUser) {
                 dispatch({
                     type: "SET_USER",
@@ -33,45 +32,34 @@ const App = () => {
             }
         });
     }, []);
-    return ( <
-        Router >
-        <
-        div className = "app" >
-        <
-        Switch >
-        <
-        Route path = "/login" >
-        <
-        Login / >
-        <
-        /Route> <
-        Route path = "/checkout" >
-        <
-        Navbar / >
-        <
-        Checkout / >
-        <
-        /Route> <
-        Route path = "/payment" >
-        <
-        Navbar / >
-        <
-        Elements stripe = { promise } >
-        <
-        Payment / >
-        <
-        /Elements> <
-        /Route> <
-        Route path = "/" >
-        <
-        Navbar / >
-        <
-        Home / >
-        <
-        /Route> <
-        /Switch> <
-        /div> <
-        /Router>
+    return (
+        <Router>
+            <div className="app">
+                <Switch>
+                    <Route path="/login">
+                        <Login />
+                    </Route>
+                    <Route path="/checkout">
+                        <Navbar />
+                        <Checkout />
+                    </Route>
+                    <Route path="/payment">
+                        <Navbar />
+                        <Elements stripe={promise}>
+                            <Payment />
+                        </Elements>
+                    </Route>
+                    <Route path="/orders">
+                        <Navbar />
+                        <Orders />
+                    </Route>
+                    <Route path="/">
+                        <Navbar />
+                        <Home />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     );
 };
 export default App;
